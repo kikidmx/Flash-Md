4576673a35946354b989cdcb8757b1de3751557a
kikidmx <168574112+kikidmx@users.noreply.github.com>
2024-06-06 21:42:07 +0300

    Update set.js

--- set.js
+++ set.js
@@ -8,9 +8,9 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'FLASH-MD-WA-BOT;;;=>',
-    PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "France King",
-    NUMERO_OWNER : process.env.OWNER_NUMBER || "254757835036", 
+    PREFIXE: process.env.PREFIX || ".",
+    OWNER_NAME: process.env.OWNER_NAME || "zidi bot ",
+    NUMERO_OWNER : process.env.OWNER_NUMBER || "96878635244", 
              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
 AUTOREAD_MESSAGES: process.env.AUTO_READ_MESSAGES || "yes",
@@ -19,7 +19,7 @@ CHATBOT: process.env.CHAT_BOT || "yes",
     BOT : process.env.BOT_NAME || 'FLASH-MD',
     OPENAI_API_KEY : process.env.OPENAI_API_KEY || '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',
     URL : process.env.BOT_MENU_LINKS || 'https://static.animecorner.me/2023/08/op2.jpg',
-    MODE: process.env.PUBLIC_MODE || "no",
+    MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'no',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
     HEROKU_APY_KEY : process.env.HEROKU_API_KEY ,
